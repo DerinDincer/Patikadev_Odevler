6333b1d5f48db698bf991134c62ac29a3fb67b60
Derin Dincer <derinxdincer@gmail.com>
2022-06-11 18:32:17 +0300

    done

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a positive number: ");
+
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter " + number + " positive numbers: ");
+
+        string numbers = Convert.ToString(Console.ReadLine());
+
+        numbers = numbers.Replace(" ", "");
+
+        Console.WriteLine(numbers);
+
+        foreach (char i in numbers)
+        {
+            if (i % 2 == 0) Console.WriteLine(i);
+            else continue;
+        }
+    }
+}
