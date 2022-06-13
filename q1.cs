26deb91e9cada55c4bf191383af97d5d4813a870
Derin Dincer <derinxdincer@gmail.com>
2022-06-13 19:56:20 +0300

    q1

---
+++ q1.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a positive number: ");
+
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter " + number + " positive numbers: ");
+
+        int[] numbers = new int[number];
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        Console.WriteLine(" ");
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] % 2 == 0) Console.WriteLine(numbers[i]);
+            else continue;
+        }
+    }
+}
